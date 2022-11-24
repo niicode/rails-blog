d18d60ce308caa3de6f4658a6db2d455b03a0443
niicode <niidarku66@gmail.com>
2022-11-24 12:17:12 +0000

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def update_post_count(user_name)
     user = User.find_by(name: user_name)
     user.post_count += 1
