8a715a8027760ecb0bab997971bc6f120650e4c2
niicode <niidarku66@gmail.com>
2022-11-24 11:12:16 +0000

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :posts
-  has_many :comments, through: :posts
-  has_many :likes, through: :posts
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def three_recent_posts(user_name)
     Post.where(name: user_name).order(created_at: :desc).limit(3)
