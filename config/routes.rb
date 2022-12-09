23772a673f72759c44b2c820259c3a98ea097515
niicode <niidarku66@gmail.com>
2022-12-09 17:19:42 +0000

    Add devise changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   # Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     authenticated :user do
-      root to: 'users#index', as: :authenticated_root
+      root :to => 'users#index', as: :authenticated_root
     end
     unauthenticated :user do
-      root to: 'devise/registrations#new', as: :unauthenticated_root
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes
     end
   end
 end
